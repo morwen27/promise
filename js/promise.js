fb818c4014bb19104ad306e88b28b819e2135814
Krikunova Olga <shersh.ol@gmail.com>
2021-10-24 13:10:03 +0700

    метод race

--- js/promise.js
+++ js/promise.js
@@ -67,37 +67,77 @@ class MyPromise {
             });
         }).catch(error => error);
     }
+
+    static race(iterable) {
+        return new MyPromise((resolve, reject) => {
+            let results = [];
+
+            iterable.forEach(async(promise, index) => {
+                let resultPromise = await promise.catch(error => {
+                    if (results.length === 0) reject(error);
+                });
+                results.push(resultPromise);
+
+                if (results.length === 1) resolve(results[0]);
+            });
+        }).catch(error => error);
+    }
 }
 
 
 
-const promise = new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('RERE');
-    }, 150);
-});
+// const promise = new MyPromise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('RERE');
+//     }, 150);
+// });
 
-promise
-    .then(data => {
-        console.log('MyPromise: ', data.toLowerCase());
-        return data;
-    })
-    .then(data => {
-        console.log('MyPromise: ', data.toUpperCase());
-        return data;
-    })
-    .then(data => {
-        console.log('MyPromise: ', data.toUpperCase()[0] + data.toLowerCase().slice(1));
-        return data;
-    })
-    .catch(error => console.log('MyPromiseError: ', error))
-    .finally(() => console.log('Finally!'))
+// promise
+//     .then(data => {
+//         console.log('MyPromise: ', data.toLowerCase());
+//         return data;
+//     })
+//     .then(data => {
+//         console.log('MyPromise: ', data.toUpperCase());
+//         return data;
+//     })
+//     .then(data => {
+//         console.log('MyPromise: ', data.toUpperCase()[0] + data.toLowerCase().slice(1));
+//         return data;
+//     })
+//     .catch(error => console.log('MyPromiseError: ', error))
+//     .finally(() => console.log('Finally!'))
 
 
-MyPromise.all([
-        new MyPromise(resolve => setTimeout(() => resolve(1), 3000)), // 1
-        new MyPromise((resolve, reject) => setTimeout(() => reject(new Error("Ошибка!")), 2000)), // 2
-        new MyPromise(resolve => setTimeout(() => resolve(3), 1000)) // 3
-    ])
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+// MyPromise.all([
+//         new MyPromise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+//         new MyPromise((resolve, reject) => setTimeout(() => reject(new Error("Ошибка из all!")), 2000)), // 2
+//         new MyPromise(resolve => setTimeout(() => resolve(3), 1000)) // 3
+//     ])
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+// MyPromise.all([
+//         new MyPromise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+//         new MyPromise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+//         new MyPromise(resolve => setTimeout(() => resolve(3), 1000)) // 3
+//     ])
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+
+// MyPromise.race([
+//         new MyPromise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+//         new MyPromise((resolve, reject) => setTimeout(() => reject(new Error("Ошибка из race!")), 2000)), // 2
+//         new MyPromise(resolve => setTimeout(() => resolve(3), 1000)) // 3
+//     ])
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+// MyPromise.race([
+//         new MyPromise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+//         new MyPromise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+//         new MyPromise(resolve => setTimeout(() => resolve(3), 1000)) // 3
+//     ])
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
